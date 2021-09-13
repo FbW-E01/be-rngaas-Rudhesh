08b049b573820bbbcc388f14e4961347de60bd94
dci <rudeshdhote@gmail.com>
2021-09-13 13:40:06 +0200

    done

--- index.js
+++ index.js
@@ -8,9 +8,18 @@ console.log("API_KEY = " + process.env.PATH);
 const server = http.createServer((req, res) => {
   console.log("Request Received");
 
-  res.end(
-    `${Math.round(Math.random() * 100)} ${"API_KEY = " + process.env.API_KEY}`
-  );
+  // res.end(
+  //   `${Math.round(Math.random() * 100)} ${"API_KEY = " + process.env.API_KEY}`
+  // );
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${pro.env.API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+
+  res.end(data.main.temp);
 });
 
 server.listen(process.env.PORT);
