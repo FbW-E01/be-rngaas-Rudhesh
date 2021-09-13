60aa546b3f348c08a00c04a8f5a8f308fa69c493
dci <rudeshdhote@gmail.com>
2021-09-13 11:22:11 +0200

    change

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import http from "http";
+import dotenv from "dotenv";
 
 const server = http.createServer((req, res) => {
   console.log("Request Received");
   res.write("Random Number: ");
-  res.write(`${Math.round(Math.random() * 100)}`);
+  res.write(
+    `${Math.round(Math.random() * 100)} ${"API_KEY = " + process.env.API_KEY}`
+  );
   res.end();
 });
 
 server.listen(8081);
+console.log("server starting http://localhost:8081");
+
+dotenv.config();
+
+console.log("API_KEY = " + process.env.API_KEY);
+
+console.log("API_KEY = " + process.env.PATH);
